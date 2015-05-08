d91119905680050190648c199abf43e7e6021bda
David Cheng <davidxcheng@gmail.com>
2015-05-08 15:22:07 +0200

    run gruntfile when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
-echo heysan
-
 node --version
 npm --version
 
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
     echo Has Gruntfile
+    npm install grunt-cli
+
+    ./node_modules/.bin/grunt --no-color
 fi
 
