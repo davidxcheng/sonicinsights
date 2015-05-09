ec9486247232154d5f73866e14f6cf5be0625396
David Cheng <davidxcheng@gmail.com>
2015-05-09 09:42:56 +0200

    probe azure..

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,5 @@ fi
 
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
     echo Has Gruntfile
-    eval npm install grunt-cli
-
-    echo grunt-cli installed
-
     ./node_modules/.bin/grunt --no-color
 fi
