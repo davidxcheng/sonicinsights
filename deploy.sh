808011d6c7225f93eaf85b7ac3b8e7876bd4d51b
davidxcheng <davidxcheng@gmail.com>
2015-05-09 10:30:40 +0200

    probing azure..

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-node --version
-npm --version
-
 npm config set strict-ssl false
 
 # Enable running this script locally
@@ -17,5 +14,8 @@ fi
 
 if [ -e "$DEPLOYMENT_SOURCE/Gruntfile.js" ]; then
     echo Has Gruntfile
+    # Having both grunt and grunt-cli as devDependencies
+    # in package.json seems to cause circular dependency.
+    npm install grunt-cli
     ./node_modules/grunt-cli/bin/grunt --no-color
 fi
